634ecad23d5b09ea7ae002bc466560b60713b6d8
Martin Blazek <martinblazek6@gmail.com>
2023-10-24 18:02:12 +0200

    multiple users with same username validation

--- src/main/java/com/martin/controller/UserController.java
+++ src/main/java/com/martin/controller/UserController.java
@@ -2,8 +2,11 @@ package com.martin.controller;
 
 import com.martin.entity.User;
 import com.martin.entity.records.UserRecord;
+import com.martin.exceptions.UserAlreadyExistException;
 import com.martin.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,9 +27,13 @@ public class UserController {
     }
 
     @PostMapping({"/registerNewUser"})
-    public UserRecord registerNewUser(@RequestBody User user) {
-        user = userService.registerNewUser(user);
-        return new UserRecord(user.getUserName(),user.getUserFirstName(),user.getUserLastName(),user.getRole());
+    public ResponseEntity<UserRecord> registerNewUser(@RequestBody User user) {
+        try {
+            user = userService.registerNewUser(user);
+            return new ResponseEntity<>(new UserRecord(user.getUserName(), user.getUserFirstName(), user.getUserLastName(), user.getRole()), HttpStatus.ACCEPTED);
+        } catch (UserAlreadyExistException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_ACCEPTABLE);
+        }
     }
 
     @GetMapping({"/forAdmin"})
