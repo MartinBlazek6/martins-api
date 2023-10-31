988ca1a5bd93ab5d683bbf744dced5aca625448d
Martin Blazek <martinblazek6@gmail.com>
2023-10-31 16:56:42 +0100

    add education integrated refactor

--- src/main/java/com/martin/service/resume/ResumeService.java
+++ src/main/java/com/martin/service/resume/ResumeService.java
@@ -1,10 +1,16 @@
 package com.martin.service.resume;
 
+import com.martin.entity.candidate.Resume;
 import com.martin.entity.records.ResumeDTO;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public interface ResumeService {
 
     void saveResume(ResumeDTO resumeDTO);
+
+    <T, ID> void saveRelatedEntities(Resume resume, List<T> entities, JpaRepository<T, ID> repository);
 }
--- src/main/java/com/martin/service/resume/ResumeServiceImpl.java
+++ src/main/java/com/martin/service/resume/ResumeServiceImpl.java
@@ -10,42 +10,78 @@ import com.martin.repositories.EmploymentRepository;
 import com.martin.repositories.PersonalDetailsRepository;
 import com.martin.repositories.ResumeRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @RequiredArgsConstructor
 @Service
-public class ResumeServiceImpl implements ResumeService{
+@Slf4j
+public class ResumeServiceImpl implements ResumeService {
 
     private final ResumeRepository resumeRepository;
     private final PersonalDetailsRepository personalDetailsRepository;
     private final EmploymentRepository employmentRepository;
     private final EducationRepository educationRepository;
-    @Override
-    public void saveResume(ResumeDTO resumeDTO){
+
+//    @Override
+//    public void saveResume(ResumeDTO resumeDTO) {
+//        Resume resume = resumeDTO.getResume();
+//        PersonalDetails personalDetails = resumeDTO.getPersonalDetails();
+//
+//        Resume savedResume = resumeRepository.save(resume);
+//
+//        personalDetails.setResume(savedResume);
+//
+//        PersonalDetails savedPd = personalDetailsRepository.save(personalDetails);
+//
+//        List<Employment> employments = resumeDTO.getEmployments();
+//        for (Employment employment : employments) {
+//            employment.setResume(savedResume);
+//            employmentRepository.save(employment);
+//        }
+//
+//        List<Education> educations = resumeDTO.getEducations();
+//        for (Education education : educations) {
+//            education.setResume(savedResume);
+//            educationRepository.save(education);
+//        }
+//
+////        mapAndSaveRelatedEntities(savedResume, resumeDTO, Collections.singletonList(educations));
+////        mapAndSaveRelatedEntities(savedResume, resumeDTO, Collections.singletonList(employments));
+//
+//        savedResume.setPersonalDetails(savedPd);
+//        resumeRepository.save(savedResume);
+//    }
+
+    public void saveResume(ResumeDTO resumeDTO) {
         Resume resume = resumeDTO.getResume();
         PersonalDetails personalDetails = resumeDTO.getPersonalDetails();
 
         Resume savedResume = resumeRepository.save(resume);
-
         personalDetails.setResume(savedResume);
-
         PersonalDetails savedPd = personalDetailsRepository.save(personalDetails);
-
-        List<Employment> employments = resumeDTO.getEmployments();
-        for (Employment employment : employments) {
-            employment.setResume(savedResume);
-            employmentRepository.save(employment);
-        }
-
-        List<Education> educations = resumeDTO.getEducations();
-        for (Education education : educations) {
-            education.setResume(savedResume);
-            educationRepository.save(education);
-        }
 
         savedResume.setPersonalDetails(savedPd);
         resumeRepository.save(savedResume);
+
+        saveRelatedEntities(savedResume, resumeDTO.getEmployments(), employmentRepository);
+        saveRelatedEntities(savedResume, resumeDTO.getEducations(), educationRepository);
+    }
+
+    @Override
+    public  <T, ID> void saveRelatedEntities(Resume resume, List<T> entities, JpaRepository<T, ID> repository) {
+        for (T entity : entities) {
+            switch (entity.getClass().getSimpleName()) {
+                case "Employment" -> ((Employment) entity).setResume(resume);
+                case "Education" -> ((Education) entity).setResume(resume);
+
+                default -> {
+                }
+            }
+            repository.save(entity);
+        }
     }
 }
