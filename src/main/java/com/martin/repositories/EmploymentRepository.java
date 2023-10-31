45e9d75e517e75695821029152450779c39b9a10
Martin Blazek <martinblazek6@gmail.com>
2023-10-31 21:37:09 +0100

    stage -> main conflicts resolved

--- src/main/java/com/martin/repositories/EmploymentRepository.java
+++ src/main/java/com/martin/repositories/EmploymentRepository.java
@@ -1,5 +1,6 @@
 package com.martin.repositories;
 
+import com.martin.entity.candidate.Employment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
--- src/main/java/com/martin/repositories/PersonalDetailsRepository.java
+++ src/main/java/com/martin/repositories/PersonalDetailsRepository.java
@@ -1,6 +1,8 @@
 package com.martin.repositories;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import com.martin.entity.candidate.PersonalDetails;
 
 @Repository
 public interface PersonalDetailsRepository extends JpaRepository<PersonalDetails,Integer> {
