d495becaadbc77c4eae0aeaec13f51971166a77a
Martin Blazek <martinblazek6@gmail.com>
2023-11-03 16:11:30 +0100

    add skills and refactor flyway scripts

--- src/main/resources/db/migration/V3__create_employments_table.sql
+++ src/main/resources/db/migration/V3__create_employments_table.sql
@@ -2,7 +2,9 @@
 CREATE TABLE employment (
                              id INT AUTO_INCREMENT PRIMARY KEY,
                              project_name VARCHAR(255),
-                             description VARCHAR(255),
+                             technologies VARCHAR(255),
+                             position VARCHAR(255),
+                             duration VARCHAR(255),
                              resume_id INT,
                              FOREIGN KEY (resume_id) REFERENCES resume(id)
 );
