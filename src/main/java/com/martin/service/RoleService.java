5d9eeebc6b68e1c7c1d38afa67124ac93148930a
Martin Blazek <martinblazek6@gmail.com>
2023-11-04 20:33:02 +0100

    refactor dependency injection

--- src/main/java/com/martin/service/RoleService.java
+++ src/main/java/com/martin/service/RoleService.java
@@ -2,14 +2,15 @@ package com.martin.service;
 
 import com.martin.repositories.RoleRepository;
 import com.martin.entity.Role;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class RoleService {
 
-    @Autowired
-    private RoleRepository roleRepository;
+    private final RoleRepository roleRepository;
 
     public Role createNewRole(Role role) {
         return roleRepository.save(role);
