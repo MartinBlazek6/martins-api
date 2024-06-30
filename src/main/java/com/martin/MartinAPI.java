f68bc4e7050afa3d297ebf7edba215c1e147f523
Martin Blazek <martinblazek6@gmail.com>
2024-06-30 21:03:40 +0200

    swagger implementation

--- src/main/java/com/martin/MartinAPI.java
+++ src/main/java/com/martin/MartinAPI.java
@@ -2,8 +2,10 @@ package com.martin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class MartinAPI {
 
     public static void main(String[] args) {
