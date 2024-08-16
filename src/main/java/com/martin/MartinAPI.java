fe98ee62f5dfc4360c1e5d89ecba395621d43be3
Martin Blažek <89529362+MartinBlazek6@users.noreply.github.com>
2024-08-16 22:20:29 +0200

    Stage (#43)
    * ResumeEmployment done
    
    * personal details added
    
    * multiple users with same username validation
    
    * add skills and education class
    
    * add skills and education class
    
    * add skills and CV-fields refactor
    
    * add JSON better formatting
    
    * flyway implementation finished
    
    * add sequence_table and banner
    
    * add user roles
    
    * Feature/update resume
    
    * add postman collection and updated JSON info
    
    * add description
    
    * add personal details logic is done
    
    * update unit tests
    
    * update user password (#36)
    
    * Validations (#38)
    
    * swagger implementation

--- src/main/java/com/martin/MartinAPI.java
+++ src/main/java/com/martin/MartinAPI.java
@@ -1,13 +1,33 @@
 package com.martin;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.io.IOException;
 
 @SpringBootApplication
+@EnableSwagger2
+@Slf4j
 public class MartinAPI {
-
     public static void main(String[] args) {
         SpringApplication.run(MartinAPI.class, args);
+        openHomePage();
     }
 
+    private static void openHomePage() {
+        String url = "http://localhost:8080/swagger-ui/";
+        String os = System.getProperty("os.name").toLowerCase();
+
+        try {
+            if (os.contains("win")) {
+                new ProcessBuilder("rundll32", "url.dll,FileProtocolHandler", url).start();
+            } else if (os.contains("mac")) {
+                new ProcessBuilder("open", url).start();
+            }
+        } catch (IOException e) {
+            log.error("An error occurred while trying to open the browser", e);
+        }
+    }
 }
--- src/main/java/com/martin/controller/JwtController.java
+++ src/main/java/com/martin/controller/JwtController.java
@@ -5,17 +5,30 @@ import com.martin.entity.JwtRequest;
 import com.martin.entity.JwtResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 
 @RestController
 @CrossOrigin
 @RequestMapping("/api")
+@Api(tags = "JWT Authentication API")
 public class JwtController {
 
-    @Autowired
-    private JwtService jwtService;
+    private final JwtService jwtService;
 
-    @PostMapping({"/authenticate"})
-    public JwtResponse createJwtToken(@RequestBody JwtRequest jwtRequest) throws Exception {
+    @Autowired
+    public JwtController(JwtService jwtService) {
+        this.jwtService = jwtService;
+    }
+
+    @ApiOperation(value = "Generate JWT Token",
+            notes = "Authenticate user and generate a JWT token",
+            response = JwtResponse.class)
+    @PostMapping("/authenticate")
+    public JwtResponse createJwtToken(
+            @ApiParam(value = "JWT Request object containing username and password", required = true)
+            @RequestBody JwtRequest jwtRequest) throws Exception {
         return jwtService.createJwtToken(jwtRequest);
     }
 }
